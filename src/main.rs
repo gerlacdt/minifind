09d24d06ac7490da7a8cc7a81b1175f443a7ffbb
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2022-11-14 19:26:23 +0100

    better user error message

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
+use anyhow::Result;
 use clap::Parser;
 use minifind::clap::Args;
 use minifind::{find, Options};
 use regex::Regex;
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
+    match exec() {
+        Ok(()) => Ok(println!("")),
+        Err(e) => Ok(eprintln!("ERROR executing minifind, cause: {}", e)),
+    }
+}
+
+fn exec() -> Result<()> {
     let args = Args::parse();
-    let pattern = args.pattern.map(|pattern| Regex::new(&pattern).unwrap());
+    let pattern = args.pattern.map(|pattern| Regex::new(&pattern));
+    let pattern = match pattern {
+        Some(p) => Some(p?),
+        None => None,
+    };
+
     let opts = Options {
         directory: args.directory,
         pattern,
